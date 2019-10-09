adf42988e07eb62db2d1148ee9cc5db41ba62ef6
Ethan Brenner <ethanlbrenner@gmail.com>
2019-10-09 16:56:09 -0500

    fixed correct answer formatting error

--- Exercise2/Quiz.php
+++ Exercise2/Quiz.php
@@ -11,15 +11,37 @@
         }
 	
 	echo "<br><br>" . "Question 2: What class do EECS students get to skip that others do not?" . "<br>";
-        echo "You answered: " . $_POST["class"] . "<br>";
-        echo "Correct answer: Engineering Ethics";
+        echo "You answered: " . $_POST["course"] . "<br>";
+        echo "Correct answer: Engineering Ethics" . "<br>";
 
         if($_POST["class"] == "Engineering Ethics"){
             $counter = $counter + 1;
         }
 
-        echo "Question 3: What are the primary colors?";
+        echo "<br><br>" . "Question 3: What are the primary colors?" . "<br>";
+	echo "You answered: " . $_POST["colors"] . "<br>";
+	echo "Correct answer: Red Blue Yellow" . "<br>";
+	
+	if($_POST["colors"] == "Red Blue Yellow"){
+            $counter = $counter + 1;
+        }
 
+	echo "<br><br>" . "Question 4: 10*2+6/3=? (Hint: PEMDAS)" . "<br>";
+        echo "You answered: " . $_POST["exp"] . "<br>";
+        echo "Correct answer: 22" . "<br>";
+
+        if($_POST["exp"] == "22"){
+            $counter = $counter + 1;
+        }
+	
+	echo "<br><br>" . "Question 5: What is the color of Donald Duck's Bowtie?" . "<br>";
+        echo "You answered: " . $_POST["bowColor"] . "<br>";
+        echo "Correct answer: Red" . "<br>";
+
+        if($_POST["bowColor"] == "Red"){
+            $counter = $counter + 1;
+        }
+	
     }
 
     printer();
