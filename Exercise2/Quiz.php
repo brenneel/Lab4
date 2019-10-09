8a6008b1525f1aab6f5b860a22cdf3f8e1f2969d
Ethan Brenner <ethanlbrenner@gmail.com>
2019-10-09 16:47:40 -0500

    changed name of question 2 radio button

--- Exercise2/Quiz.php
+++ Exercise2/Quiz.php
@@ -1,28 +1,25 @@
 <?php
-    $counter = 0;
+   
 
     function printer(){
-        echo "Question 1: What topping does NOT go on a pizza?";
-
-        echo "You answered: ";
-        echo $_POST["topping"];
-
+ 	$counter = 0;
+        echo "Question 1: What topping does NOT go on a pizza?" . "<br>";
+        echo "You answered: " . $_POST["topping"] . "<br>";
         echo "Correct answer: Pineapple";
         if($_POST["topping"] == "Pineapple"){
             $counter = $counter + 1;
         }
-
-        echo "Question 2: What class do EECS students get to skip that others do not?";
-
-        echo "You answered: ";
-        echo $_POST["class"];
-
+	
+	echo "<br><br>" . "Question 2: What class do EECS students get to skip that others do not?" . "<br>";
+        echo "You answered: " . $_POST["class"] . "<br>";
         echo "Correct answer: Engineering Ethics";
+
         if($_POST["class"] == "Engineering Ethics"){
             $counter = $counter + 1;
         }
 
-        echo "Question 3: What are the primary colors?"
+        echo "Question 3: What are the primary colors?";
+
     }
 
     printer();
