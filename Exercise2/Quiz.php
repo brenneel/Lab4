d04b9c5c6368eee800729401e9318281cd22f051
Ethan <ethanlbrenner@gmail.com>
2019-10-09 16:34:43 -0500

    fix echo

--- Exercise2/Quiz.php
+++ Exercise2/Quiz.php
@@ -3,14 +3,14 @@
 
     function printer(){
         echo "Question 1: What topping does NOT go on a pizza?";
-        echo "You answered: " $_POST["topping"];
+        echo "You answered: " . $_POST["topping"];
         echo "Correct answer: Pineapple";
         if($_POST["topping"] == "Pineapple"){
             $counter = $counter + 1;
         }
 
         echo "Question 2: What class do EECS students get to skip that others do not?";
-        echo "You answered: " $_POST["class"];
+        echo "You answered: " . $_POST["class"];
         echo "Correct answer: Engineering Ethics";
         if($_POST["class"] == "Engineering Ethics"){
             $counter = $counter + 1;
