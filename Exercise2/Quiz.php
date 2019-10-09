b46799115f40e820bb36b8314f9cabe467f2c508
Ethan <ethanlbrenner@gmail.com>
2019-10-09 16:37:03 -0500

    fix 2 for echo

--- Exercise2/Quiz.php
+++ Exercise2/Quiz.php
@@ -2,9 +2,23 @@
     $counter = 0;
 
     function printer(){
-        
+        echo "Question 1: What topping does NOT go on a pizza?";
+        echo "You answered: ";
         echo $_POST["topping"];
+        echo "Correct answer: Pineapple";
+        if($_POST["topping"] == "Pineapple"){
+            $counter = $counter + 1;
+        }
 
+        echo "Question 2: What class do EECS students get to skip that others do not?";
+        echo "You answered: ";
+        echo $_POST["class"];
+        echo "Correct answer: Engineering Ethics";
+        if($_POST["class"] == "Engineering Ethics"){
+            $counter = $counter + 1;
+        }
+
+        echo "Question 3: What are the primary colors?"
     }
 
     printer();
