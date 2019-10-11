efe9ede99272e172957f60e1d3168af98ed15997
Ethan Brenner <ethanlbrenner@gmail.com>
2019-10-10 22:12:59 -0500

    Fixed form js validation before submitting

--- Exercise3/formChecker.js
+++ Exercise3/formChecker.js
@@ -1,13 +1,39 @@
 //form checker code goes here
 
-
-function submitForm(){
+/*
+function validate(){
     console.log("submit worked");
-}
+    //validation stuff and things
+    isValid = true;
+    if(document.getElementById("username").value == ""){
+        isValid = false;
+        alert("Please enter a username");
 
+    }
+    else{
+        document.getElementById("myForm").submit();
+    }
+ 
+  
+}
+*/
 
 //resets the form to it's initial state
 function refreshForm(){
     console.log("page reset");
     location.href = "./frontEnd.html";
+}
+
+function validate(){
+    console.log("submit worked");
+    //validation stuff and things
+    isValid = true;
+    if(document.getElementById("username").value == ""){
+        isValid = false;
+        alert("Please enter a username");
+        return(false);
+    }
+    alert("passed");
+    return(true);
+ 
 }
