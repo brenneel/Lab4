5b6297905565adf539888fb9392bae12e158f463
Ethan Brenner <ethanlbrenner@gmail.com>
2019-10-10 22:44:38 -0500

    input validation now works

--- Exercise3/formChecker.js
+++ Exercise3/formChecker.js
@@ -1,4 +1,47 @@
 //form checker code goes here
+function validate(){
+    console.log("submit worked");
+    //validation stuff and things
+    let isValid = true;
+    if(document.getElementById("username").value == ""){
+        alert("Please enter a username");
+        return(false);
+    }
+    if(document.getElementById("password").value == ""){
+        alert("Please enter a password");
+        return(false);
+    }
+   var checker = checkboxChecker();
+   if(checker == false){
+       return(false);
+   }
+
+    return(true);
+    
+ 
+}
+
+function checkboxChecker(){
+    var check = document.getElementsByTagName("input");
+    var isChecked = false;
+    for(i=0; i<check.length; i++){
+       
+        if(check[i].type == 'radio' && (check[i].checked ==true)){
+            isChecked = true;
+        }
+        
+    }
+    if(isChecked == false){
+        alert("Please select a shipping option");
+    }
+    return(isChecked);
+}
+
+//resets the form to it's initial state
+function refreshForm(){
+    console.log("page reset");
+    location.href = "./frontEnd.html";
+}
 
 /*
 function validate(){
@@ -17,23 +60,3 @@ function validate(){
   
 }
 */
-
-//resets the form to it's initial state
-function refreshForm(){
-    console.log("page reset");
-    location.href = "./frontEnd.html";
-}
-
-function validate(){
-    console.log("submit worked");
-    //validation stuff and things
-    isValid = true;
-    if(document.getElementById("username").value == ""){
-        isValid = false;
-        alert("Please enter a username");
-        return(false);
-    }
-    alert("passed");
-    return(true);
- 
-}
