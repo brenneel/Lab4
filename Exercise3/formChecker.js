39b6bb7d9a1d8e934a594250903e97aa09e6647d
Ethan Brenner <ethanlbrenner@gmail.com>
2019-10-10 20:22:16 -0500

    testing js

--- Exercise3/formChecker.js
+++ Exercise3/formChecker.js
@@ -1,1 +1,14 @@
 //form checker code goes here
+
+
+
+
+function submitForm(){
+    console.log("submit worked");
+}
+
+//resets the form to it's initial state
+function refreshForm(){
+    console.log("worked");
+    location.href = "./frontEnd.html";
+}
